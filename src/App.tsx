c1d64a1c82f74095ce2653bc0ee9cbe6e7e3610b
shansr-prog <bdshan26@gmail.com>
2025-03-24 22:05:20 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -182,7 +182,7 @@ function App() {
       <footer className="bg-white">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
           <p className="text-center text-gray-500">
-            ©{new Date().getFullYear()} - 2024 <a href="https://shansr.netlify.app"> Md Shahin. </a> <br> <hr> </hr>All rights reserved.
+            © {new Date().getFullYear()} - 2024 <a href="https://shansr.netlify.app"> Md Shahin. </a> <br> <hr>All rights reserved.
           </p>
         </div>
       </footer>
